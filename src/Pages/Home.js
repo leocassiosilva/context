9af6bda61cc61f0f55051141ef9f6c974f3b0d96
leocassiosilva <leocassiosilva1234@gmail.com>
2021-08-18 21:06:22 -0300

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from './pages/Home';
+
+const Stack = createStackNavigator();
+
+export function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name='Home' component={Home}  options={{ headerShown: false  }}  />
+        </Stack.Navigator>
+    );
+}
